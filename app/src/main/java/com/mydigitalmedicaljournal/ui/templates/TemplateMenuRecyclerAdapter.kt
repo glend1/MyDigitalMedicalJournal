c602342990d6aed68b534d7a2de57a5469e9678c
Glen <psycotik666@gmail.com>
2020-05-07 12:13:40 +0100

    tidied up some recycler adapters

--- app/src/main/java/com/mydigitalmedicaljournal/ui/templates/TemplateMenuRecyclerAdapter.kt
+++ app/src/main/java/com/mydigitalmedicaljournal/ui/templates/TemplateMenuRecyclerAdapter.kt
@@ -12,16 +12,14 @@ import com.mydigitalmedicaljournal.ui._generics.ViewHolder
 
 class TemplateMenuRecyclerAdapter(private val namedResourceList: MutableList<NamedResource>, private val navController: NavController) : RecyclerView.Adapter<ViewHolder>() {
 
-    private lateinit var context: Context
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        context = parent.context
         val layout = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
         return ViewHolder(layout)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.itemView.findViewById<TextView>(R.id.text).text = namedResourceList[position].getName(context)
+        val textView = holder.itemView.findViewById<TextView>(R.id.text)
+        textView.text = namedResourceList[position].getName(textView.context)
         if (namedResourceList[position].resource != null){
             holder.itemView.setOnClickListener {
                 navController.navigate(namedResourceList[position].resource!!)
