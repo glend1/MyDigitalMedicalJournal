d9f48fd4e7b1a01053d72675caf620796d47fbd7
Glen <psycotik666@gmail.com>
2020-06-21 16:08:19 +0100

    navigation broke after doing writing tests, it was fixed. old element was creating nested views with the same ID.

--- app/src/main/java/com/mydigitalmedicaljournal/MainActivity.kt
+++ app/src/main/java/com/mydigitalmedicaljournal/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -36,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         val drawerLayout: DrawerLayout? = findViewById(R.id.drawer_layout)
         isTablet(drawerLayout)
-        //setupNav(drawerLayout)
+        setupNav(drawerLayout)
     }
 
     private fun databaseConnect() {
@@ -52,7 +52,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupNav(drawerLayout: DrawerLayout?) {
         val navView: NavigationView = findViewById(R.id.nav_view)
-        navController = findNavController(R.id.nav_host_fragment)
+        val fragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        navController = fragment!!.findNavController()
+        //navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val navSet = setOf(
