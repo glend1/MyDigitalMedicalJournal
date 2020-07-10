c584e2f730ea5942a2c8daf49e282d65ec6c6de4
Glen <psycotik666@gmail.com>
2020-07-10 16:52:37 +0100

    done some basic file tests, added a todo list to remember what to do next

---
+++ app/src/androidTest/java/com/mydigitalmedicaljournal/instrumentTests/file/FileHelperTests.kt
@@ -0,0 +1,48 @@
+package com.mydigitalmedicaljournal.instrumentTests.file
+
+import androidx.test.platform.app.InstrumentationRegistry
+import com.mydigitalmedicaljournal.json.FileHelper
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Assert.assertFalse
+import org.junit.Before
+import org.junit.Test
+
+class FileHelperTests {
+    //TODO Categories
+    //TODO TemplateManager
+    //TODO directory interactions
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+    private lateinit var file: FileHelper
+    private lateinit var testText: String
+
+    @Before
+    fun setup() {
+        file = FileHelper("test", context)
+        testText = "test"
+        file.write(testText)
+    }
+
+    @After
+    fun teardown() {
+        file.delete()
+    }
+
+    @Test
+    fun fileCreation() {
+        assertTrue(file.exists())
+    }
+
+    @Test
+    fun fileContents() {
+        val fileText = file.read()
+        assertEquals(testText, fileText)
+    }
+
+    @Test
+    fun fileDeleted() {
+        file.delete()
+        assertFalse(file.exists())
+    }
+}
