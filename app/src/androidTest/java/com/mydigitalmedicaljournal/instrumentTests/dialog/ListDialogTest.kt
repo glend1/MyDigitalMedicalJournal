152cb770b029fe73d5835a61099ec30db839eb2c
Glen <psycotik666@gmail.com>
2020-08-16 13:09:14 +0100

    added list dialog test

---
+++ app/src/androidTest/java/com/mydigitalmedicaljournal/instrumentTests/dialog/ListDialogTest.kt
@@ -0,0 +1,44 @@
+package com.mydigitalmedicaljournal.instrumentTests.dialog
+
+import android.content.Context
+import android.content.DialogInterface
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.RootMatchers.isDialog
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.activityScenarioRule
+import com.mydigitalmedicaljournal.MainActivity
+import com.mydigitalmedicaljournal.R
+import com.mydigitalmedicaljournal.template.file.TemplateList
+import com.mydigitalmedicaljournal.ui._generics.dialogs.ListDialog
+import com.mydigitalmedicaljournal.ui.categories.ManageCategoriesAdapter
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import java.util.*
+
+class ListDialogTest {
+    @get:Rule
+    val activityScenarioRule = activityScenarioRule<MainActivity>()
+
+    @Test
+    fun listDialogTest() {
+        var dialog: ListDialog?
+        val testVal = "test2"
+        val testUuid = UUID.fromString("93c9ca2a-627d-495d-a67f-e6e6c38afe3b")
+        val list = listOf(
+            TemplateList.FileList("test", UUID.fromString("f8145027-8a4d-4f9b-b4b5-2ebf049983e3")),
+            TemplateList.FileList(testVal, testUuid),
+            TemplateList.FileList("test3", UUID.fromString("99cb8d9a-26f1-4bd8-8959-84fba7367151"))
+        )
+        val adapter = ManageCategoriesAdapter(list, mutableListOf())
+        activityScenarioRule.scenario.onActivity { activity: MainActivity? ->
+            dialog = ListDialog("test", "test", activity as Context, adapter)
+            dialog!!.setConfirm(DialogInterface.OnClickListener { _, _ -> })
+            dialog!!.show()
+        }
+        onView(withText(testVal)).inRoot(isDialog()).perform(click())
+        onView(withText(R.string.Yes)).inRoot(isDialog()).perform(click())
+        assertEquals(adapter.localData[0], testUuid)
+    }
+}
