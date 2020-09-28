8d11ebe44b0ddd7807fc03d77bc3221273e03934
Glen <psycotik666@gmail.com>
2020-09-28 12:28:59 +0100

    refactored template list adapter

--- app/src/main/java/com/mydigitalmedicaljournal/ui/templates/ManageTemplatesFragment.kt
+++ app/src/main/java/com/mydigitalmedicaljournal/ui/templates/ManageTemplatesFragment.kt
@@ -15,13 +15,7 @@ class ManageTemplatesFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_templates, container, false)
-        val template = TemplateList.getTemplates(requireContext())
-        val viewAdapter =
-            TemplateListAdapter(
-                template,
-                R.layout.list_item,
-                findNavController()
-            )
+        val viewAdapter = TemplateListAdapter(TemplateList.getTemplates(requireContext()))
         val templateList = root.findViewById<RecyclerView>(R.id.template_recycler)
         val itemDecoration = CustomDivider(requireContext())
         templateList.addItemDecoration(itemDecoration)
--- app/src/main/java/com/mydigitalmedicaljournal/ui/templates/TemplateListAdapter.kt
+++ app/src/main/java/com/mydigitalmedicaljournal/ui/templates/TemplateListAdapter.kt
@@ -4,26 +4,27 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.os.bundleOf
-import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.mydigitalmedicaljournal.R
 import com.mydigitalmedicaljournal.template.file.TemplateList
 import com.mydigitalmedicaljournal.ui._generics.ViewHolder
 
-class TemplateListAdapter(private var fileList: List<TemplateList.FileList>, val layout: Int, private val navController: NavController) : RecyclerView.Adapter<ViewHolder>() {
+class TemplateListAdapter(var fileList: List<TemplateList.FileList>) : RecyclerView.Adapter<ViewHolder>() {
     override fun getItemCount() = fileList.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layout = LayoutInflater.from(parent.context).inflate(layout, parent, false)
+        val layout = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
         return ViewHolder(layout)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val title = holder.itemView.findViewById<TextView>(R.id.text)
+        val item = holder.itemView
+        val title = item.findViewById<TextView>(R.id.text)
         title.text = fileList[position].name
-        holder.itemView.setOnClickListener {
+        item.setOnClickListener {
             val bundle = bundleOf("data" to fileList[position].id)
-            navController.navigate(R.id.editorFragment, bundle)
+            item.findNavController().navigate(R.id.editorFragment, bundle)
         }
     }
 }
