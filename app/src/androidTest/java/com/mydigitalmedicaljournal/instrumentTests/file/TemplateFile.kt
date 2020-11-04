12a13d6026d7480a7e16e427f1d0d4e5e5fc2c9a
Glen <psycotik666@gmail.com>
2020-11-04 14:02:47 +0000

    fixed imports

--- app/src/androidTest/java/com/mydigitalmedicaljournal/instrumentTests/file/TemplateFile.kt
+++ app/src/androidTest/java/com/mydigitalmedicaljournal/instrumentTests/file/TemplateFile.kt
@@ -7,7 +7,6 @@ import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
-import java.util.*
 
 class TemplateFile {
     //TODO include assertions on data entry for the template
