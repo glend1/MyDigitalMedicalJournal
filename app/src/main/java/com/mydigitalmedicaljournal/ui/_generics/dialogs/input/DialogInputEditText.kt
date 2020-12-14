dd0cba7584af6a65a253cf4440836b75c134d820
Glen <psycotik666@gmail.com>
2020-12-14 13:22:08 +0000

    updated text entry fields

--- app/src/main/java/com/mydigitalmedicaljournal/ui/_generics/dialogs/input/DialogInputEditText.kt
+++ app/src/main/java/com/mydigitalmedicaljournal/ui/_generics/dialogs/input/DialogInputEditText.kt
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AlertDialog
 class DialogInputEditText(builder: AlertDialog.Builder, context : Context, text: String) : AbstractDialogInput {
     private val input = EditText(context)
     init {
-        input.inputType = InputType.TYPE_TEXT_FLAG_AUTO_CORRECT + InputType.TYPE_TEXT_FLAG_CAP_SENTENCES
+        input.inputType = InputType.TYPE_TEXT_FLAG_AUTO_COMPLETE + InputType.TYPE_TEXT_FLAG_CAP_WORDS
         input.setText(text)
         input.selectAll()
         builder.setView(input)
