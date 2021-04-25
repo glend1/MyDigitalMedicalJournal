ca58274b32871d33164dd25d4c63cf35c41f4ff9
Glen <psycotik666@gmail.com>
2021-04-25 12:34:16 +0100

    refactored and added error handling for FieldEditorFragment.kt

--- app/src/main/java/com/mydigitalmedicaljournal/ui/templates/editor/field/FieldEditorFragment.kt
+++ app/src/main/java/com/mydigitalmedicaljournal/ui/templates/editor/field/FieldEditorFragment.kt
@@ -4,46 +4,68 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.mydigitalmedicaljournal.R
 import com.mydigitalmedicaljournal.template.TemplateEnum
 import com.mydigitalmedicaljournal.template.file.TemplateManager
 import java.util.*
 
 class FieldEditorFragment : Fragment() {
-    private lateinit var enumType: TemplateEnum
-    private lateinit var template: TemplateManager
+    private var enumType: TemplateEnum? = null
+    private var template: TemplateManager? = null
+    private var error: Int? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val position = arguments?.get("position") as Int?
         val filename = arguments?.get("filename") as UUID?
         val type = arguments?.get("type") as TemplateEnum?
-        val layout = if (filename != null) {
-            template = TemplateManager(requireContext(), filename)
-            when {
-                position != null -> {
-                    val field = template.getData().getData(position)
-                    enumType = field.type
-                    enumType.editorLayout
-                }
-                type != null -> {
-                    enumType = type
-                    enumType.editorLayout
-                }
-                else -> {
-                    0
-                }
-            }
-
+        val layout = getLayout(filename, position, type)
+        return if (layout != null) {
+            layoutSetup(inflater, layout, container, position)
         } else {
-            0
+            layoutError(inflater, container)
         }
-        val view = inflater.inflate(layout, container, false)
-        (activity as? AppCompatActivity)?.supportActionBar?.title = requireContext().resources.getString(enumType.listName)
-        enumType.createEditor(view, template, position)
+    }
+
+    private fun layoutError(inflater: LayoutInflater, container: ViewGroup?): View {
+        val view = inflater.inflate(R.layout.empty_recycler, container, false)
+        view.findViewById<TextView>(R.id.message).text = requireContext().resources.getString(error!!)
         return view
     }
 
-    fun save() {
+    private fun layoutSetup(inflater: LayoutInflater, layout: Int, container: ViewGroup?, position: Int?): View {
+        val view = inflater.inflate(layout, container, false)
+        (activity as? AppCompatActivity)?.supportActionBar?.title = requireContext().resources.getString(enumType!!.listName)
+        enumType!!.createEditor(view, template!!, position)
+        return view
     }
+
+    private fun getLayout(filename: UUID?, position: Int?, type: TemplateEnum?): Int? {
+        return if (filename != null) {
+            template = TemplateManager(requireContext(), filename)
+            setEnumType(position, type)
+            enumType!!.editorLayout
+        } else {
+            error = R.string.filename_notset
+            null
+        }
+    }
+
+    private fun setEnumType(position: Int?, type: TemplateEnum?) {
+        enumType = when {
+            position != null -> {
+                template!!.getData().getData(position).type
+            }
+            type != null -> {
+                type
+            }
+            else -> {
+                error = R.string.enum_notset
+                null
+            }
+        }
+    }
+
 }
