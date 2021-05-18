22f1e56b51461f39ecb65bd93c8fdb9525b14a6b
Glen <psycotik666@gmail.com>
2021-05-18 12:34:05 +0100

    added support for description entry

--- app/src/main/java/com/mydigitalmedicaljournal/template/fields/data/DataDescription.kt
+++ app/src/main/java/com/mydigitalmedicaljournal/template/fields/data/DataDescription.kt
@@ -1,12 +1,42 @@
 package com.mydigitalmedicaljournal.template.fields.data
 
 import android.content.Context
+import com.mydigitalmedicaljournal.R
 import com.mydigitalmedicaljournal.template.TemplateEnum
 
 class DataDescription: GenericData() {
     override val type = TemplateEnum.DESCRIPTION
-    override fun listDisplay(context: Context): String = "TempName"
+    companion object {
+        const val QUESTION_FIELD = R.string.question_field
+        const val QUESTION_LENGTH = R.string.QUESTION_LENGTH
+        const val QUESTION_SYMBOLS = R.string.QUESTION_SYMBOLS
+        const val QUESTION_NOT_FOUND = R.string.QUESTION_NOT_FOUND
+    }
+
+    override fun listDisplay(context: Context): String = "${context.getString(type.listName)} : $question"
+    private var questionError: Int? = null
+
+    var question: String? = null
+        set(value) {
+            if (value.isNullOrBlank()) {
+                questionError = QUESTION_NOT_FOUND
+            } else if (Regex("^[\\w\\s\\d]+\$").containsMatchIn(value)) {
+                if (value.length <= 80) {
+                    field = value
+                    questionError = null
+                } else {
+                    questionError = QUESTION_LENGTH
+                }
+            } else {
+                questionError = QUESTION_SYMBOLS
+            }
+        }
+
     override fun validate(): MutableMap<Int, Int> {
-        return mutableMapOf()
+        val errors = mutableMapOf<Int, Int>()
+        if (questionError != null) {
+            errors[QUESTION_FIELD] = questionError!!
+        }
+        return errors
     }
 }
