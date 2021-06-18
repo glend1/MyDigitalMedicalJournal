e32865e0dcf6e466f4a0cf94b1739bd8ce029c1c
Glen <psycotik666@gmail.com>
2021-06-18 12:31:59 +0100

    tidied up some comments. added checkbox support for editor

--- app/src/main/java/com/mydigitalmedicaljournal/template/fields/data/question/DataCheck.kt
+++ app/src/main/java/com/mydigitalmedicaljournal/template/fields/data/question/DataCheck.kt
@@ -1,11 +1,45 @@
 package com.mydigitalmedicaljournal.template.fields.data.question
 
+import com.mydigitalmedicaljournal.R
 import com.mydigitalmedicaljournal.template.TemplateEnum
 import com.mydigitalmedicaljournal.template.fields.data.GenericQuestionData
+import com.mydigitalmedicaljournal.template.fields.editor.SortableEditorAdapter
 
 class DataCheck: GenericQuestionData() {
+    companion object {
+        const val CHECK_LENGTH = R.string.CHECK_LENGTH
+        const val CHECK_SYMBOLS = R.string.CHECK_SYMBOLS
+        const val CHECK_NOT_FOUND = R.string.CHECK_NOT_FOUND
+        const val CHECK_NOT_ENOUGH = R.string.CHECK_NOT_ENOUGH
+    }
+
     override val type = TemplateEnum.CHECK
+    val data = mutableListOf<String>()
+    var checkError: Int? = null
+
     override fun validateAfterQuestion(errors: MutableMap<Int, Int?>) {
-        TODO("Not yet implemented")
+        var total = 0
+        data.indices.forEach {
+            errors[SortableEditorAdapter.getPosition(it)] = validateString(data[it])
+            if (errors[SortableEditorAdapter.getPosition(it)] == null) {
+                total++
+            }
+        }
+        checkError = if (total >= 2) { null } else { CHECK_NOT_ENOUGH }
+        errors[R.id.check_count] = checkError
+    }
+
+    private fun validateString(str: String?): Int? {
+        return if (str.isNullOrBlank()) {
+            CHECK_NOT_FOUND
+        } else if (Regex("^[\\w\\s\\d?]+\$").containsMatchIn(str)) {
+            if (str.length <= 25) {
+                null
+            } else {
+                CHECK_LENGTH
+            }
+        } else {
+            CHECK_SYMBOLS
+        }
     }
 }
--- app/src/main/java/com/mydigitalmedicaljournal/template/fields/editor/question/EditorCheck.kt
+++ app/src/main/java/com/mydigitalmedicaljournal/template/fields/editor/question/EditorCheck.kt
@@ -1,16 +1,45 @@
 package com.mydigitalmedicaljournal.template.fields.editor.question
 
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.mydigitalmedicaljournal.R
+import com.mydigitalmedicaljournal.template.fields.data.question.DataCheck
+import com.mydigitalmedicaljournal.template.fields.data.question.DataRadio
 import com.mydigitalmedicaljournal.template.fields.editor.GenericQuestionEditor
+import com.mydigitalmedicaljournal.template.fields.editor.SortableEditorAdapter
 import com.mydigitalmedicaljournal.template.file.TemplateManager
+import com.mydigitalmedicaljournal.ui._generics.CustomDivider
 
 class EditorCheck(view: View, template: TemplateManager, position: Int?): GenericQuestionEditor(view, template, position) {
+    val data = if (position != null) {
+        template.getData().getData(position) as DataCheck
+    } else {
+        val newData = DataCheck()
+        template.getData().add(newData)
+        newData
+    }
+    private val checkCount: TextView = view.findViewById(R.id.check_error)
+    private val addCheck: Button = view.findViewById(R.id.addCheck)
+    private val checkRecycler: RecyclerView = view.findViewById(R.id.check_recycler)
 
-    override fun setData() {
-        TODO("Not yet implemented")
+    init {
+        setup(data)
     }
 
+    override fun setData() {}
+
     override fun setInitialData() {
-        TODO("Not yet implemented")
+        errorTextViews[R.id.check_count] = checkCount
+        val viewAdapter = SortableEditorAdapter(data.data, errorTextViews)
+        if (data.data.isEmpty()) {
+            viewAdapter.add("")
+            viewAdapter.add("")
+        }
+        val itemDecoration = CustomDivider(view.context)
+        checkRecycler.addItemDecoration(itemDecoration)
+        checkRecycler.adapter = viewAdapter
+        addCheck.setOnClickListener { viewAdapter.add("") }
     }
 }
