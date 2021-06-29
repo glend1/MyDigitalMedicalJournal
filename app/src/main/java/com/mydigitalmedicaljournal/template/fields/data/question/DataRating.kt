0e355fcb5b7dfb5cb2d6092a523034762f01a09d
Glen <psycotik666@gmail.com>
2021-06-29 12:25:04 +0100

    data field complete

--- app/src/main/java/com/mydigitalmedicaljournal/template/fields/data/question/DataRating.kt
+++ app/src/main/java/com/mydigitalmedicaljournal/template/fields/data/question/DataRating.kt
@@ -1,11 +1,58 @@
 package com.mydigitalmedicaljournal.template.fields.data.question
 
+import com.mydigitalmedicaljournal.R
 import com.mydigitalmedicaljournal.template.TemplateEnum
 import com.mydigitalmedicaljournal.template.fields.data.GenericQuestionData
 
 class DataRating: GenericQuestionData() {
+    companion object {
+        const val MIN_RATING_SIZE = R.string.MIN_RATING_SIZE
+        const val MAX_RATING_SIZE = R.string.MAX_RATING_SIZE
+        const val MIN_RATING_NOT_FOUND = R.string.MIN_RATING_NOT_FOUND
+        const val MAX_RATING_NOT_FOUND = R.string.MAX_RATING_NOT_FOUND
+        const val MIN_MAX_NOT_VALID = R.string.MIN_MAX_NOT_VALID
+    }
+
     override val type = TemplateEnum.RATING
+    private var minError: Int? = null
+    var minVal: Int? = null
+        set(value) {
+            minError = validateNumber(value)
+            if (minError == null) { field = value }
+            validateRange()
+        }
+    private var maxError: Int? = null
+    var maxVal: Int? = null
+        set(value) {
+            maxError = validateNumber(value)
+            if (maxError == null) { field = value }
+            validateRange()
+        }
+    private var rangeError: Int? = null
     override fun validateAfterQuestion(errors: MutableMap<Int, Int?>) {
-        TODO("Not yet implemented")
+        errors[R.id.max_error] = maxError
+        errors[R.id.min_error] = minError
+        errors[R.id.min_max_range] = rangeError
+    }
+
+    private fun validateRange() {
+        rangeError = null
+        if (minVal != null && maxVal != null) {
+            if (minVal!! >= maxVal!!) {
+                rangeError = MIN_MAX_NOT_VALID
+            }
+        }
+    }
+
+    private fun validateNumber(value: Int?): Int? {
+        //TODDO this needs to be worked after i have refactoed strings.xml
+        if (value == null) {
+            return MIN_RATING_NOT_FOUND
+        } else {
+            if (value > 1000 || value < 0) {
+                return MIN_RATING_SIZE
+            }
+        }
+        return null
     }
 }
--- app/src/main/java/com/mydigitalmedicaljournal/template/fields/editor/question/EditorRating.kt
+++ app/src/main/java/com/mydigitalmedicaljournal/template/fields/editor/question/EditorRating.kt
@@ -1,16 +1,50 @@
 package com.mydigitalmedicaljournal.template.fields.editor.question
 
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import com.mydigitalmedicaljournal.R
+import com.mydigitalmedicaljournal.template.fields.data.question.DataRating
 import com.mydigitalmedicaljournal.template.fields.editor.GenericQuestionEditor
 import com.mydigitalmedicaljournal.template.file.TemplateManager
 
 class EditorRating(view: View, template: TemplateManager, position: Int?): GenericQuestionEditor(view, template, position) {
+    val data = if (position != null) {
+        template.getData().getData(position) as DataRating
+    } else {
+        val newData = DataRating()
+        template.getData().add(newData)
+        newData
+    }
+    private val maxField = view.findViewById<EditText>(R.id.maximum)
+    private val minField = view.findViewById<EditText>(R.id.minimum)
+    private val maxError = view.findViewById<TextView>(R.id.error_max)
+    private val minError = view.findViewById<TextView>(R.id.error_min)
+    private val minMaxError = view.findViewById<TextView>(R.id.error_min_max)
+    //TODO add (optional) labels recycler
+
+    init {
+        setup(data)
+    }
 
     override fun setData() {
-        TODO("Not yet implemented")
+        data.maxVal = editTextToInt(maxField)
+        data.minVal = editTextToInt(minField)
     }
 
     override fun setInitialData() {
-        TODO("Not yet implemented")
+        errorTextViews[R.id.max_error] = maxError
+        errorTextViews[R.id.min_error] = minError
+        errorTextViews[R.id.min_max_range] = minMaxError
+        minField.setText(data.minVal?.toString())
+        maxField.setText(data.maxVal?.toString())
+    }
+
+    private fun editTextToInt(et: EditText) : Int? {
+        return try {
+            et.text.toString().toInt()
+        } catch (e: Exception) {
+            null
+        }
     }
 }
